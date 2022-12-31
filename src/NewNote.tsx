140bd5da0a8b058f8f60fc431f7b541f42a07196
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2022-12-31 15:46:37 +0530

    Add create note functionality and store the tags and notes in localStorage

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,25 @@
+import { NoteData, Tag } from "./App";
 import { NoteForm } from "./NoteForm";
 
-export const NewNote = () => {
+type NewNoteprops = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export const NewNote = ({
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NewNoteprops) => {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 };
